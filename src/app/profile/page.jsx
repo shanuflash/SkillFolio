4570bdd8bb08b2bcb93584a93718bdc727d502ff
Shanu Flash <shanuflash@gmail.com>
2023-07-05 00:37:02 +0530

    change events for object'd data

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -33,6 +33,21 @@ const page = () => {
     setData((prev) => ({ ...prev, [name]: e.target.innerText }));
   };
 
+  const handleChangeObj = (e, name) => {
+    const key = name.split(".");
+    setData((prev) => ({
+      ...prev,
+      [key[0]]: { [key[1]]: e.target.innerText },
+    }));
+  };
+
+  const handleChangeObjIndex = (e, name, i) => {
+    const key = name.split(".");
+    const test = data[key[0]];
+    test[i][key[1]] = e.target.innerText;
+    setData((prev) => ({ ...prev, [key[0]]: test }));
+  };
+
   const handleEdit = async () => {
     setEdit((prev) => !prev);
     if (edit) {
@@ -53,13 +68,12 @@ const page = () => {
         </div>
         <div className={styles.content}>
           <div className={styles.name}>
-            {data?.name}{" "}
+            {data?.name || "your name"}{" "}
             <span
               className={styles.profession}
               contentEditable={edit}
-              suppressContentEditableWarning={true}
               dangerouslySetInnerHTML={{
-                __html: data?.designation || "edit this",
+                __html: data?.designation || "your designation",
               }}
               onBlur={(e) => handleChange(e, "designation")}
             />
@@ -67,21 +81,18 @@ const page = () => {
           <div
             className={styles.description}
             contentEditable={edit}
-            suppressContentEditableWarning={true}
             dangerouslySetInnerHTML={{
-              __html: data?.description || "edit this",
+              __html: data?.description || "your description",
             }}
             onBlur={(e) => handleChange(e, "description")}
           />
-
           <div className={styles.contact}>
             <div className={styles.item}>
               Address:{" "}
               <span
                 contentEditable={edit}
-                suppressContentEditableWarning={true}
                 dangerouslySetInnerHTML={{
-                  __html: data?.address || "edit this",
+                  __html: data?.address || "your address",
                 }}
                 onBlur={(e) => handleChange(e, "address")}
               />
@@ -90,9 +101,8 @@ const page = () => {
               Phone:{" "}
               <span
                 contentEditable={edit}
-                suppressContentEditableWarning={true}
                 dangerouslySetInnerHTML={{
-                  __html: data?.phone || "edit this",
+                  __html: data?.phone || "your phone",
                 }}
                 onBlur={(e) => handleChange(e, "phone")}
               />
@@ -101,9 +111,8 @@ const page = () => {
               Email:{" "}
               <span
                 contentEditable={edit}
-                suppressContentEditableWarning={true}
                 dangerouslySetInnerHTML={{
-                  __html: data?.email || "edit this",
+                  __html: data?.email || "your email",
                 }}
                 onBlur={(e) => handleChange(e, "email")}
               />
@@ -112,9 +121,8 @@ const page = () => {
               Date of Birth:{" "}
               <span
                 contentEditable={edit}
-                suppressContentEditableWarning={true}
                 dangerouslySetInnerHTML={{
-                  __html: data?.dob || "edit this",
+                  __html: data?.dob || "your dob",
                 }}
                 onBlur={(e) => handleChange(e, "dob")}
               />
@@ -137,12 +145,19 @@ const page = () => {
               >
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
               </svg>
-              <a
+              {/* <a
                 href={"https://www.linkedin.com/in/" + data?.socials?.linkedin}
                 target="_blank"
               >
                 {data?.socials?.linkedin}
-              </a>
+              </a> */}
+              <span
+                contentEditable={edit}
+                dangerouslySetInnerHTML={{
+                  __html: data?.socials?.linkedin || "your linkedin",
+                }}
+                onBlur={(e) => handleChangeObj(e, "socials.linkedin")}
+              />
             </div>
             <div className={styles.item}>
               <svg
@@ -154,22 +169,58 @@ const page = () => {
               >
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
-              <a
+              {/* <a
                 href={"https://github.com/" + data?.socials?.github}
                 target="_blank"
               >
                 {data?.socials?.github}
-              </a>
+              </a> */}
+              <span
+                contentEditable={edit}
+                dangerouslySetInnerHTML={{
+                  __html: data?.socials?.github || "your github",
+                }}
+                onBlur={(e) => handleChangeObj(e, "socials.github")}
+              />
             </div>
           </div>
           <div className={styles.section}>
             <div className={styles.title}>Education</div>
-            {data?.education?.map((item) => (
-              <div className={styles.item}>
-                <div className={styles.name}>{item.name}</div>
-                <div className={styles.description}>{item.description}</div>
+            {data?.education?.map((item, index) => (
+              <div className={styles.item} key={index}>
+                <span
+                  contentEditable={edit}
+                  dangerouslySetInnerHTML={{
+                    __html: item.name || "education name",
+                  }}
+                  onBlur={(e) =>
+                    handleChangeObjIndex(e, "education.name", index)
+                  }
+                />
+                <span
+                  contentEditable={edit}
+                  dangerouslySetInnerHTML={{
+                    __html: item.description || "year and grades",
+                  }}
+                  onBlur={(e) =>
+                    handleChangeObjIndex(e, "education.description", index)
+                  }
+                />
               </div>
             ))}
+            <button
+              onClick={() =>
+                setData((prev) => ({
+                  ...prev,
+                  education: [
+                    ...prev.education,
+                    { name: "education name", description: "year and grades" },
+                  ],
+                }))
+              }
+            >
+              Add Education
+            </button>
           </div>
           <div className={styles.section}>
             <div className={styles.title}>Skills</div>
