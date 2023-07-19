8148804dbccdef675c76e46f5832c45e6ea89c3c
Shanu Flash <shanuflash@gmail.com>
2023-07-19 23:58:27 +0530

    fix production

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${location.origin}/auth/callback`,
       },
     });
   };
