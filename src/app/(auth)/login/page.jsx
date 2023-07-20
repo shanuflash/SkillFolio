390d5d109dd33e94b7b68b076518e4c59d21653c
Shanu Flash <shanuflash@gmail.com>
2023-07-20 11:19:12 +0530

    test

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: "https://skillfolio.vercel.app//auth/callback",
       },
     });
   };
