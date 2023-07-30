0eb00865a62ad3652d5a538c65270693c78c45c2
Shanu Flash <shanuflash@gmail.com>
2023-07-30 22:30:07 +0530

    123

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://skillfolio-lxe4nzgb5-shanuflash.vercel.app/";
+    : "https://skillfolio-git-test1-shanuflash.vercel.app/";
