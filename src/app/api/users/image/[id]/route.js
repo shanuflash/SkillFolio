8aa7440ba763bcfbde8a2bd0c3cf9d19c0fa8dfb
Shanu Flash <shanuflash@gmail.com>
2023-07-30 23:16:43 +0530

    revert

--- src/app/api/users/image/[id]/route.js
+++ src/app/api/users/image/[id]/route.js
@@ -4,7 +4,6 @@ import dbConnection from "../../../utils/db";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import { writeFile } from "fs/promises";
-import { join } from "path";
 
 dbConnection(process.env.NEXT_PUBLIC_MONGO_URL);
 cloudinary.config({
@@ -22,26 +21,25 @@ export async function POST(req, { params }) {
     const file = formData.get("file");
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    const path = join(`/tmp/${file.name}`);
-    console.log(file, path);
+    const path = process.cwd() + "/public/" + file.name;
     if (file) {
       try {
         await writeFile(path, buffer);
-        const { secure_url } = await cloudinary.uploader.upload(path, {
-          public_id: id,
-          folder: "profile",
-        });
-        fs.unlinkSync(path);
-        await UserDetails.findByIdAndUpdate(
-          { _id: id },
-          { photo: secure_url },
-          {
-            new: true,
-          }
-        );
       } catch (error) {
         return NextResponse.json({ message: error.message }, { status: 500 });
       }
+      const { secure_url } = await cloudinary.uploader.upload(path, {
+        public_id: id,
+        folder: "profile",
+      });
+      fs.unlinkSync(path);
+      await UserDetails.findByIdAndUpdate(
+        { _id: id },
+        { photo: secure_url },
+        {
+          new: true,
+        }
+      );
       return NextResponse.json(
         { message: "Profile photo updated successfully", secure_url },
         { status: 200 }
